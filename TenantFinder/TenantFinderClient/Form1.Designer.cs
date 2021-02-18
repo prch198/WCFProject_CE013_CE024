8773b50a8312dd2f202c9114dea3f21762046a17
zalak8102 <zalakbhingradiya@gmail.com>
2021-02-18 15:36:48 +0530

    client

---
+++ TenantFinder/TenantFinderClient/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace TenantFinderClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.no1 = new System.Windows.Forms.TextBox();
+            this.name1 = new System.Windows.Forms.TextBox();
+            this.area1 = new System.Windows.Forms.TextBox();
+            this.city1 = new System.Windows.Forms.TextBox();
+            this.rent1 = new System.Windows.Forms.TextBox();
+            this.category1 = new System.Windows.Forms.TextBox();
+            this.req1 = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.edit = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.msg = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(291, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Houses";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(836, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "HouseNo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(836, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Flat/Soc Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(836, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Area";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(836, 190);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "City";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(836, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Rent";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(836, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(127, 20);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Category(house)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(836, 331);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 20);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Requirement(Tenant)";
+            // 
+            // no1
+            // 
+            this.no1.Location = new System.Drawing.Point(994, 51);
+            this.no1.Name = "no1";
+            this.no1.Size = new System.Drawing.Size(100, 26);
+            this.no1.TabIndex = 9;
+            // 
+            // name1
+            // 
+            this.name1.Location = new System.Drawing.Point(994, 92);
+            this.name1.Name = "name1";
+            this.name1.Size = new System.Drawing.Size(100, 26);
+            this.name1.TabIndex = 10;
+            // 
+            // area1
+            // 
+            this.area1.Location = new System.Drawing.Point(994, 138);
+            this.area1.Name = "area1";
+            this.area1.Size = new System.Drawing.Size(100, 26);
+            this.area1.TabIndex = 11;
+            // 
+            // city1
+            // 
+            this.city1.Location = new System.Drawing.Point(994, 184);
+            this.city1.Name = "city1";
+            this.city1.Size = new System.Drawing.Size(100, 26);
+            this.city1.TabIndex = 12;
+            // 
+            // rent1
+            // 
+            this.rent1.Location = new System.Drawing.Point(994, 228);
+            this.rent1.Name = "rent1";
+            this.rent1.Size = new System.Drawing.Size(100, 26);
+            this.rent1.TabIndex = 13;
+            // 
+            // category1
+            // 
+            this.category1.Location = new System.Drawing.Point(994, 277);
+            this.category1.Name = "category1";
+            this.category1.Size = new System.Drawing.Size(100, 26);
+            this.category1.TabIndex = 14;
+            // 
+            // req1
+            // 
+            this.req1.Location = new System.Drawing.Point(994, 325);
+            this.req1.Name = "req1";
+            this.req1.Size = new System.Drawing.Size(100, 26);
+            this.req1.TabIndex = 15;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(799, 392);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(93, 43);
+            this.save.TabIndex = 16;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // edit
+            // 
+            this.edit.Location = new System.Drawing.Point(955, 392);
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(90, 43);
+            this.edit.TabIndex = 17;
+            this.edit.Text = "Edit";
+            this.edit.UseVisualStyleBackColor = true;
+            this.edit.Click += new System.EventHandler(this.edit_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(1110, 392);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(82, 43);
+            this.delete.TabIndex = 18;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 98);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(682, 300);
+            this.dataGridView2.TabIndex = 19;
+            this.dataGridView2.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView2_CellMouseClick);
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.Location = new System.Drawing.Point(955, 13);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(0, 20);
+            this.msg.TabIndex = 20;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(936, 453);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(133, 36);
+            this.reset.TabIndex = 21;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1204, 488);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.edit);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.req1);
+            this.Controls.Add(this.category1);
+            this.Controls.Add(this.rent1);
+            this.Controls.Add(this.city1);
+            this.Controls.Add(this.area1);
+            this.Controls.Add(this.name1);
+            this.Controls.Add(this.no1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox no1;
+        private System.Windows.Forms.TextBox name1;
+        private System.Windows.Forms.TextBox area1;
+        private System.Windows.Forms.TextBox city1;
+        private System.Windows.Forms.TextBox rent1;
+        private System.Windows.Forms.TextBox category1;
+        private System.Windows.Forms.TextBox req1;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button edit;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ TenantFinder/TenantFinderClient/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TenantFinderClient.ServiceReference1;
+
+namespace TenantFinderClient
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        public void refreshForm()
+        {
+            TenantFinderClient.ServiceReference1.HouseServiceClient proxy = new TenantFinderClient.ServiceReference1.HouseServiceClient("BasicHttpBinding_IHouseService");
+            DataSet ds = proxy.GetAllHouses();
+            dataGridView2.DataSource = ds;
+            dataGridView2.DataMember = ds.Tables[0].TableName.ToString();
+            proxy.Close();
+            save.Enabled = true;
+            edit.Enabled = false;
+            delete.Enabled = false;
+            no1.Clear();
+            name1.Clear();
+            area1.Clear();
+            city1.Clear();
+            category1.Clear();
+            rent1.Clear();
+            req1.Clear();
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            bool flag=false;
+            try
+            {
+                TenantFinderClient.ServiceReference1.HouseServiceClient proxy = new TenantFinderClient.ServiceReference1.HouseServiceClient("BasicHttpBinding_IHouseService");
+                House h = new House();
+                h.No = int.Parse(no1.Text);
+                h.Name = name1.Text;
+                h.Area = area1.Text;
+                h.City = city1.Text;
+                h.Rent = float.Parse(rent1.Text);
+                h.ReqTenant = req1.Text;
+                h.Category = category1.Text;
+                flag = proxy.AddHouse(h);
+            }
+            catch(Exception ex)
+            {
+                msg.Text = "Enter valid details.";
+            }
+            if(flag)
+            {
+                msg.Text = "Successfully Added.";
+            }
+            else
+            {
+                msg.Text += "Failed to Add.";
+            }
+            refreshForm();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshForm(); 
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            
+            TenantFinderClient.ServiceReference1.HouseServiceClient proxy = new TenantFinderClient.ServiceReference1.HouseServiceClient("BasicHttpBinding_IHouseService");
+            House h2 = null;
+            try
+            {
+                House h = new House();
+                int id = int.Parse(dataGridView2.SelectedRows[0].Cells[0].Value.ToString());
+                h.ID = id;
+                h.No = int.Parse(no1.Text);
+                h.Name = name1.Text;
+                h.Area = area1.Text;
+                h.City = city1.Text;
+                h.Rent = float.Parse(rent1.Text);
+                h.ReqTenant = req1.Text;
+                h.Category = category1.Text;
+                h2 = proxy.UpdateHouse(h);
+            }
+            catch(Exception ex)
+            {
+                msg.Text = "Enter valid details.";
+            }
+            if (h2!=null)
+            {
+                msg.Text = "Successfully Updated.";
+            }
+            else
+            {
+                msg.Text += "Failed to Update.";
+            }
+            refreshForm();
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            TenantFinderClient.ServiceReference1.HouseServiceClient proxy = new TenantFinderClient.ServiceReference1.HouseServiceClient("BasicHttpBinding_IHouseService");
+            int id = int.Parse(dataGridView2.SelectedRows[0].Cells[0].Value.ToString());
+            bool flag = proxy.DeleteHouse(id);
+            if (flag)
+            {
+                msg.Text = "Successfully deleted.";
+            }
+            else
+            {
+                msg.Text = "Failed to delete.";
+            }
+            refreshForm();
+
+        }
+
+       
+
+        private void dataGridView2_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            try
+            {
+                msg.Text = dataGridView2.SelectedRows[0].Cells[0].Value + string.Empty;
+                no1.Text = dataGridView2.SelectedRows[0].Cells[1].Value + string.Empty;
+                name1.Text = dataGridView2.SelectedRows[0].Cells[2].Value + string.Empty;
+                area1.Text = dataGridView2.SelectedRows[0].Cells[3].Value + string.Empty;
+                city1.Text = dataGridView2.SelectedRows[0].Cells[4].Value + string.Empty;
+                category1.Text = dataGridView2.SelectedRows[0].Cells[5].Value + string.Empty;
+                rent1.Text = dataGridView2.SelectedRows[0].Cells[6].Value + string.Empty;
+                req1.Text = dataGridView2.SelectedRows[0].Cells[7].Value + string.Empty;
+
+                save.Enabled = false;
+                edit.Enabled = true;
+                delete.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                msg.Text = "Select row by clicking first cell.";
+            }
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            refreshForm();
+        }
+    }
+}
